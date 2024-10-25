8e98aea57d1c2eb115a9c83f525ec696033e1654
Heliacaa <121296219+Heliacaa@users.noreply.github.com>
2024-10-25 13:12:59 +0300

    Initial commit

---
+++ src/main/java/com/example/shoppingcartdeneme/ShoppingCartDenemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartdeneme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartDenemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartDenemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingcartdeneme/ShoppingCartDenemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartdeneme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDenemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
